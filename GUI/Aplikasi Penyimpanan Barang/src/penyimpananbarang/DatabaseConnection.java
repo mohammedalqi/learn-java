6e83563353c5606780890ac217498245e16de7ad
Muhammad Alqi Fahrezi <mohammedalqifahrezi@gmail.com>
2021-06-11 00:11:35 +0700

    Aplikasi Penyimpanan Barang

---
+++ GUI/Aplikasi Penyimpanan Barang/src/penyimpananbarang/DatabaseConnection.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penyimpananbarang;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Muhammad Alqi
+ */
+public class DatabaseConnection {
+    private Connection DatabaseConnection;
+    
+    public Connection connect(){
+      try{
+          Class.forName("com.mysql.cj.jdbc.Driver");
+          System.out.println("Berhasil Koneksi");
+         }  catch (ClassNotFoundException ex){
+             System.out.println("Gagal Koneksi"+ ex);
+         }  
+      
+      String url = "jdbc:mysql://localhost:3306/db_loginform";
+      try{
+          DatabaseConnection = DriverManager.getConnection(url,"root","");
+          System.out.println("Berhasil Koneksi Database");
+         }  catch (SQLException ex){
+             System.out.println("Gagal Koneksi Database"+ ex);
+         }
+        return DatabaseConnection;
+      
+    }
+}
